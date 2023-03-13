106fbe5a6e225f604bc62d069fa643b24733dc56
bai.jingfeng <bai.jingfeng@foxmail.com>
2023-03-14 03:22:27 +0800

    chore: 调整文件结构

---
+++ JavaStudy/study1/demo1.java
@@ -0,0 +1,10 @@
+package com.baizixv
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr = { 11, 5, 7, 2, 8, 9, 6 };
+        for(int i = 0; i < arr.length;i++ ) {
+            System.out.println(arr[i]);
+        }
+    }
+}
