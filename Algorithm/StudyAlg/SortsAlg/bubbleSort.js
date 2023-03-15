dfd12764cde8b9bd9e3785864d5625d4af41a400
bai.jingfeng <bai.jingfeng@foxmail.com>
2023-03-15 18:13:38 +0800

    study: 算法学习，排序算法，冒泡排序改进

--- Algorithm/StudyAlg/SortsAlg/bubbleSort.js
+++ Algorithm/StudyAlg/SortsAlg/bubbleSort.js
@@ -2,21 +2,16 @@
  * 冒泡排序，找出相对大数，不断交换位置，放到最后
  */
 import { swapFunc, isRightFunc } from '../tools/basic.js'
-const testArr = [6, 4, 8, 9, 3, 1, 7, 5, 2]
+
 function bubbleSort(arr) {
-  let k = 0
-  while (k < arr.length) {
-    for (let i = 0; i < arr.length - k; i++) {
-      if (arr[i] > arr[i + 1]) {
-        swapFunc(arr, i, i + 1)
-      }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) swapFunc(arr, j, j + 1)
     }
-    k++
-    // console.log('%c Line:7 🍋 arr', 'font-size:18px;color:#7f2b82;background:#ffdd4d', arr)
   }
   return arr
 }
-
+// const testArr = [6, 4, 8, 9, 3, 1, 7, 5, 2]
 // console.log(
 //   '%c Line:20 🍏 bubbleSort(testArr)',
 //   'font-size:18px;color:#4fff4B;background:#b03734',
