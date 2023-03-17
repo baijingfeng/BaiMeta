781dbbd5ad4aca7d19a48b6ec75a595e76c43e85
bai.jingfeng <bai.jingfeng@foxmail.com>
2023-03-17 15:48:01 +0800

    update

--- Algorithm/StudyAlg/others/dict.py
+++ Algorithm/StudyAlg/others/dict.py
@@ -1,9 +1,13 @@
 # 哈希表，散列表，字典，关联数组
 book = dict()
+mylist = {}
 
 book["apple"] = 1.2
 book["milk"] = 5.6
 
+mylist["a"] = 3
+mylist["b"] =76
+
 book["nothing"] = "fds"
 
-print(book)
+print(book,mylist)
