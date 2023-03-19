7aaf44e80f7864dab9809313a5fdd6171fb92a22
bai.jingfeng <bai.jingfeng@foxmail.com>
2023-03-19 18:09:19 +0800

    study: 算法，python, 插入排序, 改进不必两两交换

--- Algorithm/StudyAlg/SortsAlg/insertSort.py
+++ Algorithm/StudyAlg/SortsAlg/insertSort.py
@@ -1,15 +1,19 @@
 # 插入排序
-from tools import swap
+__all__ = ["insertSort"]
 
 
 def insertSort(arr):
     for i in range(1, len(arr)):
+        temp = arr[i]
+        k = 0
         for j in range(i, 0, -1):
-            if arr[j] < arr[j - 1]:
-                swap(arr, j, j - 1)
+            if temp < arr[j - 1]:
+                arr[j] = arr[j - 1]
             else:
+                k = j
                 break
-    return
+        arr[k] = temp
+    return arr
 
 
 print(insertSort([6, 4, 8, 9, 3, 1, 7, 5, 2]))
