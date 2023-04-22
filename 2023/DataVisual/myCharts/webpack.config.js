805afdc5a1494f643d829518a4feab031020c874
bai.jingfeng <bai.jingfeng@foxmail.com>
2023-04-22 14:43:33 +0800

    chore: myCharts，基本工程化配置

---
+++ 2023/DataVisual/myCharts/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'chart.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 5030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+}
